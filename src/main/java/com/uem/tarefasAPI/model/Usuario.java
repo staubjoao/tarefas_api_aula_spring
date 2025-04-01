a0aa0cf6470d490e17766b04472c522123337960
staubjoao <joao.staub42@gmail.com>
2025-04-01 09:20:05 -0300

    feat: add Usuario class

--- src/main/java/com/uem/tarefasAPI/model/Usuario.java
+++ src/main/java/com/uem/tarefasAPI/model/Usuario.java
@@ -1,4 +1,24 @@
 package com.uem.tarefasAPI.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuarios")
+@Data
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
 }
